ed92cea131927a8209fff25b577d21b94da2a252
Jonathan Barrow <jonbarrow1998@gmail.com>
2025-10-01 18:10:53 -0400

    chore(eslint): ignore no-restricted-imports in eslint.config.mjs

--- eslint-config/eslint.config.mjs
+++ eslint-config/eslint.config.mjs
@@ -1,4 +1,4 @@
-import config from './index.mjs';
+import config from './index.mjs'; // eslint-disable-line no-restricted-imports -- Will told me to ignore this here, signed - Jon
 
 export default [
 	...config
